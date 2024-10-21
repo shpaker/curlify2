2d73ecbdcfb3774c43b5f915ef4e872cee3f86d0
A.Shpak <aleksander.shpak.als@gmail.com>
2024-10-21 18:21:36 +0300

    fix: minor fixes and async read client support

--- curlify2/curlify.py
+++ curlify2/curlify.py
@@ -16,9 +16,10 @@ class Curlify:
         Returns:
             str: return string of set headers
         """
-        headers = [f'"{k}: {v}"' for k, v in self.req.headers.items()]
-
-        return " -H ".join(headers)
+        headers = " -H ".join([f'"{k}: {v}"' for k, v in self.req.headers.items()])
+        if headers:
+            headers = f"-H {headers}"
+        return headers
 
     def body(self):
         if hasattr(self.req, "body"):
@@ -34,17 +35,57 @@ class Curlify:
 
         return body
 
+    def make_curl_cli(self, method, headers, body):
+        cli_parts = [
+            f"curl -X {method}",
+            headers,
+            f"-d '{body}'" if body else None,
+            self.req.url,
+        ]
+        quote = ' '.join([str(entity) for entity in cli_parts if entity])
+
+        if self.compressed:
+            quote += " --compressed"
+        if not self.verify:
+            quote += " --insecure"
+        return quote
+
     def quote(self) -> str:
         """build curl command
 
         Returns:
             str: string represents curl command
         """
-        quote = f"curl -X {self.req.method} -H {self.headers()} -d '{self.body_decode()}' {self.req.url}"
+        return self.make_curl_cli(self.req.method, self.headers(), self.body_decode())
 
-        if self.compressed:
-            quote += " --compressed"
-        if not self.verify:
-            quote += " --insecure"
 
-        return quote
+class AsyncCurlify(Curlify):
+
+    async def to_curl(self) -> str:
+        """to_curl function returns a string of curl to execute in shell.
+        We accept 'requests' and 'httpx' module.
+        """
+        return await self.quote()
+
+    async def body(self):
+        if hasattr(self.req, "body"):
+            return self.req.body
+
+        return await self.req.aread()
+
+    async def body_decode(self):
+        body = await self.body()
+
+        if body and isinstance(body, bytes):
+            return body.decode()
+
+        return body
+
+    async def quote(self) -> str:
+        """build curl command
+
+        Returns:
+            str: string represents curl command
+        """
+        body = await self.body_decode()
+        return self.make_curl_cli(self.req.method, self.headers(), body)
--- tests/test_to_curl.py
+++ tests/test_to_curl.py
@@ -2,6 +2,9 @@ import httpx
 import requests
 import responses
 from curlify2 import Curlify
+import pytest
+
+from curlify2.curlify import AsyncCurlify
 
 URL = "http://127.0.0.1/json"
 
@@ -14,7 +17,7 @@ def test_must_return_string_from_requests_via_get():
 
     assert (
         curl_command.to_curl()
-        == 'curl -X GET -H "User-Agent: python-requests/2.31.0" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -d \'None\' http://127.0.0.1/json'
+        == 'curl -X GET -H "User-Agent: python-requests/2.32.3" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" http://127.0.0.1/json'
     )
 
 
@@ -26,7 +29,7 @@ def test_must_return_string_from_requests_via_post():
 
     assert (
         curl_command.to_curl()
-        == 'curl -X POST -H "User-Agent: python-requests/2.31.0" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Length: 0" -d \'None\' http://127.0.0.1/json'
+        == 'curl -X POST -H "User-Agent: python-requests/2.32.3" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Length: 0" http://127.0.0.1/json'
     )
 
 
@@ -38,7 +41,7 @@ def test_must_return_string_from_requests_via_put():
 
     assert (
         curl_command.to_curl()
-        == 'curl -X PUT -H "User-Agent: python-requests/2.31.0" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Length: 0" -d \'None\' http://127.0.0.1/json'
+        == 'curl -X PUT -H "User-Agent: python-requests/2.32.3" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Length: 0" http://127.0.0.1/json'
     )
 
 
@@ -50,7 +53,7 @@ def test_must_return_string_from_requests_via_delete():
 
     assert (
         curl_command.to_curl()
-        == 'curl -X DELETE -H "User-Agent: python-requests/2.31.0" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Type: application/json" -H "Content-Length: 0" -d \'None\' http://127.0.0.1/json'
+        == 'curl -X DELETE -H "User-Agent: python-requests/2.32.3" -H "Accept-Encoding: gzip, deflate" -H "Accept: */*" -H "Connection: keep-alive" -H "Content-Type: application/json" -H "Content-Length: 0" http://127.0.0.1/json'
     )
 
 
@@ -64,9 +67,7 @@ def test_must_return_string_from_httpx_via_get(httpx_mock):
         breakpoint
         assert (
             curl
-            == 'curl -X GET -H "host: 127.0.0.1" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" -d '
-            "'b''' "
-            "http://127.0.0.1/json"
+            == 'curl -X GET -H "host: 127.0.0.1" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" http://127.0.0.1/json'
         )
 
 
@@ -108,5 +109,65 @@ def test_must_return_string_from_httpx_via_delete(httpx_mock):
 
         assert (
             curl_command
-            == 'curl -X DELETE -H "host: 127.0.0.1" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" -d \'b\'\'\' http://127.0.0.1/json?id=1'
+            == 'curl -X DELETE -H "host: 127.0.0.1" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" http://127.0.0.1/json?id=1'
+        )
+
+
+@pytest.mark.asyncio
+async def test_must_return_string_from_async_httpx_via_get(httpx_mock):
+    httpx_mock.add_response()
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(URL)
+
+        curl = await AsyncCurlify(response.request).to_curl()
+        breakpoint
+        assert (
+            curl
+            == 'curl -X GET -H "host: 127.0.0.1" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" http://127.0.0.1/json'
+        )
+
+
+@pytest.mark.asyncio
+async def test_must_return_string_from_async_httpx_via_post(httpx_mock):
+    httpx_mock.add_response()
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(URL, data={"name": "marcus"})
+
+        curl = await AsyncCurlify(response.request).to_curl()
+
+        assert (
+            curl
+            == 'curl -X POST -H "host: 127.0.0.1" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" -H "content-length: 11" -H "content-type: application/x-www-form-urlencoded" -d \'name=marcus\' http://127.0.0.1/json'
+        )
+
+
+@pytest.mark.asyncio
+async def test_must_return_string_from_async_httpx_via_put(httpx_mock):
+    httpx_mock.add_response()
+
+    async with httpx.AsyncClient() as client:
+        response = await client.put(URL, data={"id": 1, "name": "almeida"})
+
+        curl_command = await AsyncCurlify(response.request).to_curl()
+
+        assert (
+            curl_command
+            == 'curl -X PUT -H "host: 127.0.0.1" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" -H "content-length: 17" -H "content-type: application/x-www-form-urlencoded" -d \'id=1&name=almeida\' http://127.0.0.1/json'
+        )
+
+
+@pytest.mark.asyncio
+async def test_must_return_string_from_async_httpx_via_delete(httpx_mock):
+    httpx_mock.add_response()
+
+    async with httpx.AsyncClient() as client:
+        response = await client.delete(URL, params={"id": 1})
+
+        curl_command = await AsyncCurlify(response.request).to_curl()
+
+        assert (
+            curl_command
+            == 'curl -X DELETE -H "host: 127.0.0.1" -H "accept: */*" -H "accept-encoding: gzip, deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.24.1" http://127.0.0.1/json?id=1'
         )
